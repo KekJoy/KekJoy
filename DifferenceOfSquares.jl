25d77cc321ac02d4c1403017828add6c1da30306
KekJoy <valiullin.anton2003@yandex.ru>
2021-10-22 13:58:52 +0500

    final

--- DifferenceOfSquares.jl
+++ DifferenceOfSquares.jl
@@ -5,7 +5,5 @@ for i = 1:maxNumber
     global squareOfTheSum += i
     global sumOfTheSquare += i^2
 end
-global squareOfTheSum = squareOfTheSum^2
-result = squareOfTheSum-sumOfTheSquare
-println(result)
+println(squareOfTheSum^2-sumOfTheSquare)
 
